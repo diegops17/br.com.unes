4dcf0dac2cc0cc2734589aa4d055a4c69489071a
diegops17 <psdiego17@gmail.com>
2023-10-11 21:43:51 -0300

    criação da página quem-somos

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/quem-somos')
+def quem_somos():
+    return render_template('quem-somos.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
